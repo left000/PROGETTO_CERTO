78fc3ed1530b11aa041f6dd71054fca15694f02c
Lorenzo Eduardo Furio <eduardofurio22@gmail.com>
2025-06-17 20:45:54 +0200

    [FIX] - Method saveCertificatiFile

--- certificato.c
+++ certificato.c
@@ -128,7 +128,7 @@ void stampaListaCertificati(ListaCertificati lista)
       }
     }
 
-    printf("\n📄 Certificato ID: %d\n", c.id_certificato);
+    printf("\nCertificato ID: %d\n", c.id_certificato);
     printf("Numero seriale: %s\n", c.numero_seriale);
     printf("Data emissione: %02d/%02d/%04d\n",
            c.data_emissione.giorno, c.data_emissione.mese, c.data_emissione.anno);
@@ -138,34 +138,34 @@ void stampaListaCertificati(ListaCertificati lista)
     // Stampa azienda
     if (az != NULL)
     {
-      printf("🏢 Azienda: %s (P.IVA: %s) | %s, %s\n",
+      printf("Azienda: %s (P.IVA: %s) | %s, %s\n",
              az->ragione_sociale, az->partita_iva, az->indirizzo, az->nazione);
     }
     else
     {
-      printf("🏢 Azienda: NON TROVATA\n");
+      printf("Azienda: NON TROVATA\n");
     }
 
     // Stampa prodotto
     if (pr != NULL)
     {
-      printf("📦 Prodotto: %s | Peso: %.2f kg | Valore: %.2f € | Codice TARIC: %s\n",
+      printf("Prodotto: %s | Peso: %.2f kg | Valore: %.2f € | Codice TARIC: %s\n",
              pr->descrizione, pr->peso_kg, pr->valore_euro, pr->codice_taric);
     }
     else
     {
-      printf("📦 Prodotto: NON TROVATO\n");
+      printf("Prodotto: NON TROVATO\n");
     }
 
     // Stampa destinazione
     if (dest != NULL)
     {
-      printf("🌍 Destinazione: %s | Porto: %s | Mezzo: %s\n",
+      printf("Destinazione: %s | Porto: %s | Mezzo: %s\n",
              dest->paese, dest->porto_arrivo, dest->mezzo_trasporto);
     }
     else
     {
-      printf("🌍 Destinazione: NON TROVATA\n");
+      printf("Destinazione: NON TROVATA\n");
     }
 
     printf("-------------------------------------------------\n");
@@ -173,42 +173,146 @@ void stampaListaCertificati(ListaCertificati lista)
     corrente = corrente->next;
   }
 }
+
 void saveCertificatiFile(ListaCertificati lista, const char *nomeFile)
 {
+  if (lista == NULL)
+  {
+    printf("Nessun certificato presente.\n");
+    return;
+  }
+
   FILE *fp = fopen(nomeFile, "w");
   if (fp == NULL)
   {
-    printf("Errore nell'apertura del file per la scrittura.\n");
+    printf("Errore nell'apertura del file %s\n", nomeFile);
     return;
   }
 
   NodoCertificato *corrente = lista;
+
   while (corrente != NULL)
   {
     CertificatoOrigine c = corrente->dato;
-    fprintf(fp, "%d;%d;%d;%d;%02d/%02d/%04d;%s;%s;%s\n",
-            c.id_certificato,
-            c.id_azienda,
-            c.id_prodotto,
-            c.id_destinazione,
-            c.data_emissione.giorno,
-            c.data_emissione.mese,
-            c.data_emissione.anno,
-            c.numero_seriale,
-            c.stato,
-            c.camera_commercio);
+
+    Azienda *az = NULL;
+    Azienda *aziende = getAziende();
+    int nAz = getNumeroAziende();
+    for (int i = 0; i < nAz; i++)
+    {
+      if (aziende[i].id == c.id_azienda)
+      {
+        az = &aziende[i];
+        break;
+      }
+    }
+
+    Prodotto *pr = NULL;
+    Prodotto *prodotti = getProdotti();
+    int nPr = getNumeroProdotti();
+    for (int i = 0; i < nPr; i++)
+    {
+      if (prodotti[i].id == c.id_prodotto)
+      {
+        pr = &prodotti[i];
+        break;
+      }
+    }
+
+    Destinazione *dest = NULL;
+    Destinazione *destinazioni = getDestinazioni();
+    int nDest = getNumeroDestinazioni();
+    for (int i = 0; i < nDest; i++)
+    {
+      if (destinazioni[i].id == c.id_destinazione)
+      {
+        dest = &destinazioni[i];
+        break;
+      }
+    }
+
+    fprintf(fp, "\nCertificato ID: %d\n", c.id_certificato);
+    fprintf(fp, "Numero seriale: %s\n", c.numero_seriale);
+    fprintf(fp, "Data emissione: %02d/%02d/%04d\n",
+            c.data_emissione.giorno, c.data_emissione.mese, c.data_emissione.anno);
+    fprintf(fp, "Stato: %s\n", c.stato);
+    fprintf(fp, "Camera commercio: %s\n", c.camera_commercio);
+
+    if (az != NULL)
+    {
+      fprintf(fp, "Azienda: %s (P.IVA: %s) | %s, %s\n",
+              az->ragione_sociale, az->partita_iva, az->indirizzo, az->nazione);
+    }
+    else
+    {
+      fprintf(fp, "Azienda: NON TROVATA\n");
+    }
+
+    if (pr != NULL)
+    {
+      fprintf(fp, "Prodotto: %s | Peso: %.2f kg | Valore: %.2f € | Codice TARIC: %s\n",
+              pr->descrizione, pr->peso_kg, pr->valore_euro, pr->codice_taric);
+    }
+    else
+    {
+      fprintf(fp, "Prodotto: NON TROVATO\n");
+    }
+
+    if (dest != NULL)
+    {
+      fprintf(fp, "Destinazione: %s | Porto: %s | Mezzo: %s\n",
+              dest->paese, dest->porto_arrivo, dest->mezzo_trasporto);
+    }
+    else
+    {
+      fprintf(fp, "Destinazione: NON TROVATA\n");
+    }
+
+    fprintf(fp, "-------------------------------------------------\n");
+
     corrente = corrente->next;
   }
 
   fclose(fp);
-  printf("📁 Certificati salvati correttamente su '%s'.\n", nomeFile);
+  printf("Dati certificati salvati correttamente in %s\n", nomeFile);
 }
+// void saveCertificatiFile(ListaCertificati lista, const char *nomeFile)
+// {
+//   FILE *fp = fopen(nomeFile, "w");
+//   if (fp == NULL)
+//   {
+//     printf("Errore nell'apertura del file per la scrittura.\n");
+//     return;
+//   }
+
+//   NodoCertificato *corrente = lista;
+//   while (corrente != NULL)
+//   {
+//     CertificatoOrigine c = corrente->dato;
+//     fprintf(fp, "%d;%d;%d;%d;%02d/%02d/%04d;%s;%s;%s\n",
+//             c.id_certificato,
+//             c.id_azienda,
+//             c.id_prodotto,
+//             c.id_destinazione,
+//             c.data_emissione.giorno,
+//             c.data_emissione.mese,
+//             c.data_emissione.anno,
+//             c.numero_seriale,
+//             c.stato,
+//             c.camera_commercio);
+//     corrente = corrente->next;
+//   }
+
+//   fclose(fp);
+//   printf("Certificati salvati correttamente su '%s'.\n", nomeFile);
+// }
+
 void loadCertificatiFile(ListaCertificati *lista, const char *nomeFile)
 {
   FILE *fp = fopen(nomeFile, "r");
   if (fp == NULL)
   {
-    printf("⚠ File '%s' non trovato o impossibile da aprire.\n", nomeFile);
+    printf("File '%s' non trovato o impossibile da aprire.\n", nomeFile);
     return;
   }
 
@@ -228,7 +332,7 @@ void loadCertificatiFile(ListaCertificati *lista, const char *nomeFile)
     NodoCertificato *nuovo = malloc(sizeof(NodoCertificato));
     if (!nuovo)
     {
-      printf("❌ Errore allocazione memoria.\n");
+      printf("Errore allocazione memoria.\n");
       fclose(fp);
       return;
     }
@@ -239,5 +343,5 @@ void loadCertificatiFile(ListaCertificati *lista, const char *nomeFile)
   }
 
   fclose(fp);
-  printf("📂 Certificati caricati da '%s'.\n", nomeFile);
+  printf("Certificati caricati da '%s'.\n", nomeFile);
 }
